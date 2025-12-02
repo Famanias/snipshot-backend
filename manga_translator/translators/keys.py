eee5f1a688a9c1494b3776c79358637d88c84810
Famanias <haratayo@gmail.com>
2025-12-03 05:13:25 +0900

    Update keys.py

--- manga_translator/translators/keys.py
+++ manga_translator/translators/keys.py
@@ -10,4 +10,4 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '')
 OPENAI_MODEL = os.getenv('OPENAI_MODEL', 'chatgpt-4o-latest')
 
 GROQ_API_KEY = os.getenv('GROQ_API_KEY', '')
-GROQ_MODEL = os.getenv('GROQ_MODEL', 'mixtral-8x7b-32768')
+GROQ_MODEL = os.getenv('GROQ_MODEL', 'meta-llama/llama-4-maverick-17b-128e-instruct')
