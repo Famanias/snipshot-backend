62921f6cb93b4e852385cf2452bfdd02a91e2a9b
Famanias <102844234+Famanias@users.noreply.github.com>
2026-03-11 18:35:42 +0900

    Removed unwanted files to clean the repository

--- server/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Server package for SnipShot Backend
-"""
-
-# Import app only when needed to avoid circular imports
-# from .main import app
-
-__all__ = ["app"]
--- server/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Routes package
-"""
-
-from .users import router as users_router
-from .images import router as images_router
-
-__all__ = ["users_router", "images_router"]
